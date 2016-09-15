078afc22d8541a391dcee519fc5529105647057a
malirezaie <m.alirezaie@gmail.com>
2016-09-15 13:20:50 -0700

    test build
    test build

--- Main.cs
+++ Main.cs
@@ -10,6 +10,9 @@ namespace testCI
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main(args, null, "AppDelegate");
+
+			int j = 10;
+
 		}
 	}
 }
